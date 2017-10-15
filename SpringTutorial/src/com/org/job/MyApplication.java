edd87a352bae06448b263058a8c4f7da9f85eeff
KadamBrother's <KadamBrother's@DESKTOP-0NAK7EV>
2017-10-15 20:45:04 +0530

    Added AppContext and Factory

--- SpringTutorial/src/com/org/job/MyApplication.java
+++ SpringTutorial/src/com/org/job/MyApplication.java
@@ -1,23 +1,38 @@
 package com.org.job;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
 
 import com.org.consumer.ServiceConsumer;
 
 public class MyApplication {
 
 	public static void main(String[] args) {
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-				"AppContext.xml");
+		
+		//This is Big Brother of BeanFactory with addition support like AOP and Event Notification,etc.
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("AppContext.xml");
+		
+		
+		//For this it will load from root location
+		//BeanFactory factory=new XmlBeanFactory(new FileSystemResource("AppContext.xml"));
+		
 		ServiceConsumer twapp = (ServiceConsumer) context.getBean("TwApp");
-
 		twapp.processMessage("Hi ", "prashant@abc.com");
+		//ServiceConsumer twapp = (ServiceConsumer) factory.getBean("TwApp");
+		//twapp.processMessage("Hi ", "prashant@abc.com");
 		
 		ServiceConsumer fbapp = (ServiceConsumer) context.getBean("FbApp");
 		
 		fbapp.processMessage("Hi", "prashant@abc.com");
+		
+		//ServiceConsumer fbapp = (ServiceConsumer) factory.getBean("FbApp");
+		
+		//fbapp.processMessage("Hi", "prashant@abc.com");
 		// close the context
 		context.close();
+		
 
 	}
 
--- SpringTutorial/src/com/org/service/FacebookService.java
+++ SpringTutorial/src/com/org/service/FacebookService.java
@@ -1,11 +1,35 @@
 package com.org.service;
 
 public class FacebookService implements MessageService {
+	
+	private String servicename;
+	
+
+	public String getServicename() {
+		return servicename;
+	}
+
+
+
+
+	public void setServicename(String servicename) {
+		this.servicename = servicename;
+	}
+
+
+
+
+
+
+
+
+
+
 
 	@Override
 	public boolean sendMessage(String msg, String rec) {
 		
-		System.out.println(msg+" send to "+rec+" using FacebookService!!");
+		System.out.println(msg+" send to "+rec+" using "+servicename+"!!");
 		
 		return true;
 	}
